3d5992482b950188178201c1c5d7a4a50bd483e2
binit-mani <binitmanibhagat9087@gmail.com>
2026-08-29 19:55:28 +0530

    Time: 3 ms (68.06%) | Memory: 15.7 MB (39.76%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        int balance = 0;
-        for (char c : s) {
-            if (c == '(') balance++;
-            else balance--;
-            if (balance < 0) return false;
-        }
-        return balance == 0;
-    }
-    void generateAll(int n,string curr,vector<string> &s){
+    void generateS(vector<string> &s,int n,string curr,int open,int close){
         if(curr.size() == 2*n){
             s.push_back(curr);
             return;
         }
-        generateAll(n,curr+'(',s);
-        generateAll(n,curr+')',s);
+        if(open<n){
+            generateS(s,n,curr+'(',open+1,close);
+        }
+        if(close<open){
+            generateS(s,n,curr+')',open,close+1);
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> s;
-        generateAll(n,"(",s);
-        vector<string> ans;
-        for(int i = 0;i<s.size();i++){
-            if(isValid(s[i]))ans.push_back(s[i]);
-        }
-        return ans;
+        generateS(s,n,"",0,0);
+        return s;
     }
 };
